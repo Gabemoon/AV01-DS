0739eb776256dadb1ad14d3bb6619c215c751c03
Gabriel Moraes <gborjamoraes@outlook.com>
2024-10-03 17:26:01 -0300

    AV1 MOD1 DS

---
+++ avaliacao01.py
@@ -0,0 +1,5 @@
+mensagem = input("Escreva uma mensagem! ")
+dia_aniversario = int(input("Escreva o dia do mês de seu aniversário! "))
+pi = float(input("Escreva o valor de Pi! "))
+
+print(f"Sua mensagem foi: {mensagem}\nO seu dia no mês de aniversário é: {dia_aniversario}\nO valor de Pi é: {pi}")
